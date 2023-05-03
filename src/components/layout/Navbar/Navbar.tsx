f863c77de9085785faca5bc561e385844a291bde
Aleksandar Stevanovic <aleksandar.stojanovic@visit.com>
2023-05-03 16:24:00 +0200

    navbar fix

--- src/components/layout/Navbar/Navbar.tsx
+++ src/components/layout/Navbar/Navbar.tsx
@@ -97,7 +97,10 @@ const DesktopNav = () => {
   const popoverContentBgColor = useColorModeValue("white", "gray.800");
 
   return (
-    <Stack direction={"row"} spacing={4}>
+    <Stack direction={"row"} spacing={4} 
+    zIndex={20}
+    
+    >
       {NAV_ITEMS.map((navItem) => (
         <Box key={navItem.label}>
           <Popover trigger={"hover"} placement={"bottom-start"}>
