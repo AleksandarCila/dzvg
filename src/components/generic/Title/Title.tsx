2a82fe2fd693d5954c843212ab8e0dab701ee882
Aleksandar Stevanovic <aleksandar.stojanovic@visit.com>
2024-01-26 15:37:26 +0100

    updated body parsing

--- src/components/generic/Title/Title.tsx
+++ src/components/generic/Title/Title.tsx
@@ -4,14 +4,14 @@ import { FC } from "react";
 type TitleProps = {
   title: string;
   size?: "sm" | "lg";
-  padding?: number
+  padding?: number;
 };
 
-export const Title: FC<TitleProps> = ({ title, size = "lg",padding=20 }) => {
+export const Title: FC<TitleProps> = ({ title, size = "lg", padding = 20 }) => {
   const fontSize = size === "sm" ? "xl" : "3xl";
   return (
     <div style={{ padding: padding }}>
-      <Heading as="h1" fontSize={fontSize}>
+      <Heading as="h1" fontSize={fontSize} style={{ textAlign: "left" }}>
         {title}
       </Heading>
       <Divider width="100px" borderWidth={2} borderColor="primary.200" mt={5} />
--- src/pages/informacije/objava/index.tsx
+++ src/pages/informacije/objava/index.tsx
@@ -1,8 +1,9 @@
 //@ts-nocheck
 import { useGetPostById } from "@/api/hooks";
 import { PageContainer, Title } from "@/components";
-import { Image, Progress, Stack, Text } from "@chakra-ui/react";
+import { Circle, Image, Progress, Stack, Text } from "@chakra-ui/react";
 import Head from "next/head";
+import Link from "next/link";
 import { useRouter } from "next/router";
 
 const Informacija = () => {
@@ -29,8 +30,10 @@ const Informacija = () => {
         {!post || loading ? (
           <Progress size="xs" isIndeterminate colorScheme="orange" />
         ) : (
-          <Stack sx={{ gap: 2, p: 5, pb: 10, textAlign: "justify" }}>
-            <Title title={post.data.title?.[0].text} />
+          <Stack
+            sx={{ gap: 2, p: { base: 3, md: 5 }, pb: 10, textAlign: "justify" }}
+          >
+            <Title title={post.data.title?.[0].text} padding={10} />
             <Stack sx={{ justifyContent: "center" }}>
               <Image
                 src={post.data.image.url}
@@ -38,9 +41,60 @@ const Informacija = () => {
                 sx={{ height: 250, objectFit: "contain" }}
               />
             </Stack>
-            {post.data.body?.map((paragraph, ind) => (
-              <Text key={ind}>{paragraph.text}</Text>
-            ))}
+            {post.data.body?.map((paragraph, ind) => {
+              const fontWeight =
+                paragraph?.spans?.[0]?.type === "strong" ? 700 : "default";
+
+              const isListItem = paragraph.type === "list-item";
+              const isImage = paragraph.type === "image";
+              const isLink = paragraph?.spans?.[0]?.type === "hyperlink";
+
+              if (isLink) {
+                const linkData = paragraph?.spans?.[0]?.data;
+                const url = linkData?.url;
+                const target = linkData?.target;
+
+                return (
+                  <Link key={ind} href={url} target={target}>
+                    {paragraph.text}
+                  </Link>
+                );
+              }
+
+              if (isImage) {
+                const url = paragraph.url;
+
+                return (
+                  <Stack
+                    key={ind}
+                    sx={{
+                      justifyContent: "center",
+                      py: 3,
+                      alignItems: "center",
+                    }}
+                  >
+                    <Image
+                      src={url}
+                      alt={paragraph?.alt}
+                      sx={{
+                        width: "100%",
+                        maxWidth: 400,
+                        objectFit: "contain",
+                      }}
+                    />
+                  </Stack>
+                );
+              }
+              return (
+                <Text
+                  as={isListItem ? "li" : "p"}
+                  key={ind}
+                  sx={{ fontWeight }}
+                >
+                  {paragraph.text}
+                </Text>
+              );
+            })}
           </Stack>
         )}
       </PageContainer>
